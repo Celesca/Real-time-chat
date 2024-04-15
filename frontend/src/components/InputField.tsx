ad4f73cb9b45068ea53e6016ef8b5f7216c8b402
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-04-15 21:19:18 +0700

    add: HandleSubmit and Model

--- frontend/src/components/InputField.tsx
+++ frontend/src/components/InputField.tsx
@@ -2,11 +2,23 @@ import React, { useState } from 'react'
 import './InputField.css'
 import { IoMdSend } from "react-icons/io";
 
-const InputField = () => {
+interface Props {
+  messages: string[]
+  setMessages: React.Dispatch<React.SetStateAction<string[]>>
+}
+
+const InputField = ({messages, setMessages} : Props) => {
   const [text, setText] = useState<string>('');
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (text === '') return;
+    setMessages([...messages, text]);
+    setText('');
+  }
+
   return (
-    <form id="form" action="" className="py-4 w-full flex justify-center">
+    <form id="form" className="py-4 w-full flex justify-center" onSubmit={(e)=> handleSubmit(e)}>
         <input type="text" id="form_input"
         value={text} 
         placeholder=""
@@ -14,7 +26,7 @@ const InputField = () => {
         autoComplete='off'
         className="w-1/2 p-2 text-black border-2 rounded-xl outline-none text-lg"
         />
-        <button className="bg-white ml-9 p-3 rounded-xl">
+        <button className="bg-white ml-9 p-3 rounded-xl" type="submit">
           <IoMdSend size={30}/>
           </button>
     </form>
--- frontend/src/pages/ChatPage/ChatPage.tsx
+++ frontend/src/pages/ChatPage/ChatPage.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import InputField from '../../components/InputField'
 
 const ChatPage = () => {
-  return ( <>
-    <div>This is ChatPage</div>
-    <InputField/>
+
+  const [messages, setMessages] = useState<string[]>([]) 
+
+  return ( 
+    <>
+      <div className="mx-64">
+      {messages.map((message, index) => {
+        return <div key={index} className="text-black">{message}</div>
+      })}
+      </div>
+      <InputField messages={messages} setMessages={setMessages}/>
+
+
     </>
   )
 }
