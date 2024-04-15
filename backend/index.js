9ae4b35aea45bf45518ab44fed2fd46494a7c10e
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-04-15 18:07:58 +0700

    first commit

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const { createServer } = require('node:http');
+const { join } = require('node:path');
+const { Server } = require('socket.io');
+
+const port = process.env.SERVER_PORT || 3000;
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+
+dotenv.config();
+
+app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, 'index.html'));
+})
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    })
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    })
+})
+
+server.listen(port, () => {
+    console.log('Server is running on port 3000');
+})
