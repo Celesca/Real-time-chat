b2f2761c0f0fb1bfa428a25ff68bd32ff67096ee
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-04-16 21:18:13 +0700

    feat: Username

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
+import { useNavigate } from 'react-router'
 
 const Login = () => {
+
+    const navigate = useNavigate();
+
+    const [username, setUsername] = useState<string>('')
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (username.trim() === '') {
+        alert("Your name cannot be blank")
+    }
+
+    sessionStorage.setItem('username', username)
+    navigate('/chat')
+
+  }
   return (
     <div className="form-container p-8">
-        <form>
+        <form onSubmit={(e) => handleSubmit(e)}>
             <div className="flex flex-col justify-center items-center">
                 <label className="text-center font-semibold text-2xl mb-4">Username</label>
-                <input className="text-lg" type="text" id="username_input" placeholder="Enter your name" />
+                <input className="text-lg" value={username} onChange={(e) => setUsername(e.target.value)}
+                type="text" id="username_input" placeholder="Enter your name" />
                 <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold text-xl py-2 rounded w-64 mt-8">Enter the chat</button>
             </div>
         </form>
--- frontend/src/pages/LoginPage/LoginPage.tsx
+++ frontend/src/pages/LoginPage/LoginPage.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import './LoginPage.css'
 import Login from '../../components/Login'
 
@@ -10,9 +9,12 @@ const LoginPage = () => {
       <h1 className="text-center pt-12 mb-16 text-black font-semibold text-3xl">Welcome to RC Chat (Real-time Chat Application) 🌎</h1>
       
         <Login/>
-        <Link to="/chat">Chat</Link>
-
+      <footer className="footer-container">
+        <div className="text-center text-black text-lg text-center py-8">Made with ❤️ by 
+          <a href="https://github.com/Celesca" className="text-yellow-300 hover:text-white">&nbsp;Celescadev</a> 
         </div>
+      </footer>
+    </div>
     </>
   )
 }
