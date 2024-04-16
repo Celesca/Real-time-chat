3a83d9f9fc9e03f83623cb7b5e4b9becaf76ae5e
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-04-16 21:41:09 +0700

    fix: CORS

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,6 @@ io.on('connection', (socket) => {
     console.log('a user connected');
 
     socket.on('chat-message', (msg) => {
-        console.log('message: ' + msg);
         io.emit('chat-message', msg);
     })
 
